f4d161050440edafce46e9f60aa4b298f93cf41a
paull <paullopisparrilla@gmail.com>
2024-12-29 13:43:08 +0000

    Ball created.

--- src/main/java/software/ulpgc/kata7/Ball.java
+++ src/main/java/software/ulpgc/kata7/Ball.java
@@ -1,4 +1,8 @@
 package software.ulpgc.kata7;
 
-public record Ball() {
+public record Ball(double radius, double restitution) {
+
+    public static Ball create(double radius, double restitution) {
+        return new Ball(radius, restitution);
+    }
 }
