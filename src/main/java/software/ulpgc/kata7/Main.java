1966e334542ac022af809f9a9e1fcd5a9d690141
paull <paullopisparrilla@gmail.com>
2024-12-29 14:05:20 +0000

    Main completed.

--- src/main/java/software/ulpgc/kata7/Main.java
+++ src/main/java/software/ulpgc/kata7/Main.java
@@ -1,6 +1,15 @@
 package software.ulpgc.kata7;
 
+import javax.swing.*;
+
 public class Main {
-
-
+    public static void main(String[] args) {
+        Ball ball = Ball.create(20, 0.9);
+        JFrame frame = new JFrame("Ball Simulator");
+        BallSimulator simulator = new BallSimulator(9.8, 800, ball);
+        frame.add(simulator);
+        frame.setSize(1920, 1080);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
